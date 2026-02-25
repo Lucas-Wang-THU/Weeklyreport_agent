56f15542ca583311ef74838e4c4f7d762e050ad3
Lucas-Wang-THU <1275918144@qq.com>
2026-02-25 16:19:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1"
 MODEL_NAME = "arcee-ai/trinity-large-preview:free" # 或其他支持的模型
 
 # 用户信息
-USER_NAME = "王骄阳"
+USER_NAME = "***"
 
 # 路径配置
 # 自动获取本周日的日期用于文件夹命名，格式如 260119 (YYMMDD)
@@ -26,4 +26,5 @@ if not os.path.exists(REPORT_WEEKLY_DIR):
 
 FILE_NAME = f"{USER_NAME}_{date_str}周报.docx"
 FULL_REPORT_PATH = os.path.join(REPORT_WEEKLY_DIR, FILE_NAME)
+
 DATA_FILE = "plan_data.json" # 本地存储计划数据
